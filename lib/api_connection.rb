ed0c08e185f9d99f4dcfd3e2dac53c2ad27f28c4
BedaBrazil <marcio@mail.com>
2017-05-14 13:53:13 -0300

    adicionado uma mensgem de exceçao na connect

--- lib/api_connection.rb
+++ lib/api_connection.rb
@@ -6,8 +6,8 @@ module APIConnection
     token = "5e20a6e514afa82121420017cbaf867bc2fca8b13c30f76b3c179f42f10ae486"
     begin
       RestClient::Request.execute(method: :post, url: "http://api.olhovivo.sptrans.com.br/v0/Login/Autenticar?token=#{token}")
-    rescue
-     "Problema na conexão"
+    rescue RestClient::ExceptionWithResponse => err
+      puts err.response
     end
   end
 end
